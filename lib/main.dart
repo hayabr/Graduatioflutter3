3e3502c30c6f05db127e013ddd7c61349b098555
hayabr <162566275+hayabr@users.noreply.github.com>
2025-02-12 21:52:37 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,34 @@
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart'; // ✅ تأكدي من استيراد GetX
+import 'package:managermoney/app/Accounts/accounts.dart';
 import 'package:managermoney/app/auth/login.dart';
 import 'package:managermoney/app/auth/signup.dart';
 import 'package:managermoney/app/auth/success.dart';
 import 'package:managermoney/app/Transaction/home.dart';
+import 'package:managermoney/controller/user_controller.dart';
 
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  
+  Get.put(UserController());
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp( // ✅ استخدمي GetMaterialApp بدلاً من MaterialApp
       debugShowCheckedModeBanner: false,
       title: 'PHP Material App',
-      initialRoute: "login", // ✅ Ensure correct case
-      routes: {
-        "login": (context) => const Login(),
-        "signup": (context) => const Signup(),
-        "home": (context) => const Home(),
-        "success": (context) => const Success(),
-      
-        
-      },
+      initialRoute: "/login", 
+      getPages: [
+        GetPage(name: "/login", page: () => const Login()),
+        GetPage(name: "/signup", page: () => const Signup()),
+        GetPage(name: "/home", page: () => const Home()),
+        GetPage(name: "/success", page: () => const Success()),
+        GetPage(name: '/accounts', page: () => Accounts()),
+      ],
     );
   }
 }
