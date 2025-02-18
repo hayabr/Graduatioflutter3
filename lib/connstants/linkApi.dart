2eac2e9961e7e8d26f04de90a762a1a3b1af3164
hayabr <162566275+hayabr@users.noreply.github.com>
2025-02-18 23:18:15 +0300

    Add files via upload

--- lib/connstants/linkApi.dart
+++ lib/connstants/linkApi.dart
@@ -3,3 +3,17 @@ const String linkServerName = "http://10.0.2.2/GraduationProject";
 //link Auth 
 const String linkSignUp ="$linkServerName/auth/signup.php";
 const String linkLogin ="$linkServerName/auth/login.php";
+//link Accounts
+const String linkViewAccount ="$linkServerName/Accounts/view.php";
+const String linkAddAccount ="$linkServerName/Accounts/add.php";
+const String linkDeleteAccount ="$linkServerName/Accounts/delete.php";
+const String linkUpdateAccount ="$linkServerName/Accounts/update.php";
+const String linkViewAccountSummary ="$linkServerName/Accounts/viewAccounts_summary.php";
+
+//link Transactions 
+const String linkViewTransaction ="$linkServerName/Transactions/view.php";
+const String linkAddTransaction ="$linkServerName/Transactions/add.php";
+const String linkDeleteTransaction ="$linkServerName/Transactions/delete.php";
+const String linkUpdateTransaction ="$linkServerName/Transactions/update.php";
+const String linkViewTransactionSummary ="$linkServerName/Transactions/viewSummary.php";
+const String linkViewCategory ="$linkServerName/Transactions/getCategory.php";
