a40b396ba97dd2edaf01cf52c3d7e8a69cfa5abd
hayabr <162566275+hayabr@users.noreply.github.com>
2025-02-18 19:46:58 +0300

    Add files via upload

---
+++ lib/app/Transaction/addTransaction.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AddTransaction extends StatelessWidget {
+  const AddTransaction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(appBar: AppBar(
+      title: Text("AddTransations"),
+        //لاخفاء arrowback
+    ),
+    body: (Column(
+     
+    )));
+  
+  }}
