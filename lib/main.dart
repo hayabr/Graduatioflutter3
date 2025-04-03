1c9ea4c3cbef19e26288bed697300745cf854756
hayabr <162566275+hayabr@users.noreply.github.com>
2025-04-03 15:46:04 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,33 @@
+import 'package:chatappmopileapp/screens/chat_screen.dart';
+import 'package:chatappmopileapp/screens/registration_screen.dart';
+import 'package:chatappmopileapp/screens/signin_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart'; // ✅ تأكدي من استيراد GetX
-import 'package:managermoney/app/Accounts/accounts.dart';
-import 'package:managermoney/app/auth/login.dart';
-import 'package:managermoney/app/auth/signup.dart';
-import 'package:managermoney/app/auth/success.dart';
-import 'package:managermoney/app/Transaction/home.dart';
-import 'package:managermoney/controller/user_controller.dart';
+import 'screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Get.put(UserController());
-  runApp(const MyApp());
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  final _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp( // ✅ استخدمي GetMaterialApp بدلاً من MaterialApp
-      debugShowCheckedModeBanner: false,
-      title: 'PHP Material App',
-      initialRoute: "/login", 
-      getPages: [
-        GetPage(name: "/login", page: () => const Login()),
-        GetPage(name: "/signup", page: () => const Signup()),
-        GetPage(name: "/home", page: () =>  Home()),
-        GetPage(name: "/success", page: () => const Success()),
-        GetPage(name: '/accounts', page: () => Accounts()),
-      ],
-    );
+    return MaterialApp(
+        title: 'MessageMe app',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        // home: ChatScreen(),
+        initialRoute: _auth.currentUser != null ? ChatScreen.screenRoute : WelcomeScreen.screenRoute,
+        routes: {
+          WelcomeScreen.screenRoute: (context) => WelcomeScreen(),
+          SignInScreen.screenRoute: (context) => SignInScreen(),
+          RegistrationScreen.screenRoute: (context) => RegistrationScreen(),
+          ChatScreen.screenRoute: (context) => ChatScreen(),
+        });
   }
 }
-
